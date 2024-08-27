7bca6a750a6d5137ebb2b41fbbc1fa12cc05c33a
Garrett Jespersen <grj26s@login.missouristate.edu>
2024-08-27 05:59:25 +0000

    Added a class about cars!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+class Car {
+    public:
+        string make;
+        string model;
+        string color;
+        int year;
+
+        void accelerate() {
+            cout << "You are speeding up!" << endl;
+        }
+        void brake() {
+            cout << "You are slowing down!" << endl;
+        }
+        void turn() {
+            cout << "You are turning!" << endl;
+        }
+};
 
 int main() {
 
+    Car camaro;
+    camaro.make = "Chevy";
+    camaro.model = "Camaro";
+    camaro.color = "Midnight Blue";
+    camaro.year = 1969;
 
+    cout << camaro.make << endl;
+    cout << camaro.model << endl;
+    cout << camaro.color << endl;
+    cout << camaro.year << endl;
 
-
+    camaro.accelerate();
+    camaro.brake();
+    camaro.turn();
 
 
     return 0;
